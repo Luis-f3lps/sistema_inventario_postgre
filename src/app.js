77cf7696bfa19ddbd1cd0d32e746849d476eb769
Luis Gustavo B. Santiago <74880596+Luis-f3lps@users.noreply.github.com>
2024-11-11 13:56:36 -0300

    update

--- src/app.js
+++ src/app.js
@@ -1009,72 +1009,72 @@ app.post('/api/registrar_entrada', async (req, res) => {
     }
   });
   
-// Endpoint para buscar consumos
-app.get('/api/consumos', Autenticado, async (req, res) => {
-    try {
-        const { startDate, endDate, laboratorio } = req.query;
-  
-        // Validação do formato de data (YYYY-MM-DD)
-        const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
-        if ((startDate && !dateRegex.test(startDate)) || (endDate && !dateRegex.test(endDate))) {
-            return res.status(400).json({ error: 'As datas devem estar no formato YYYY-MM-DD.' });
-        }
-  
-        // Validação do intervalo de datas
-        if (startDate && endDate && new Date(startDate) > new Date(endDate)) {
-            return res.status(400).json({ error: 'A data de início não pode ser posterior à data de término.' });
-        }
-  
-        let query = `
-            SELECT 
-                rc.id_consumo, 
-                rc.data_consumo, 
-                e.sigla, 
-                e.nome_produto, 
-                l.nome_laboratorio, 
-                rc.quantidade, 
-                e.tipo_unidade_produto, 
-                rc.descricao 
-            FROM 
-                registro_consumo rc 
-            JOIN 
-                produto e ON rc.id_produto = e.id_produto 
-            JOIN 
-                laboratorio l ON rc.id_laboratorio = l.id_laboratorio
-        `;
-  
-        const params = [];
-        let whereClauses = [];
-  
-        // Filtrar por intervalo de datas
-        if (startDate && endDate) {
-            whereClauses.push('rc.data_consumo BETWEEN ? AND ?');
-            params.push(startDate, endDate);
-        }
-  
-        // Filtrar por laboratório
-        if (laboratorio && laboratorio !== 'todos') {
-            whereClauses.push('rc.id_laboratorio = ?');
-            params.push(laboratorio);
-        }
-  
-        if (whereClauses.length > 0) {
-            query += ' WHERE ' + whereClauses.join(' AND ');
-        }
-  
-        query += ' ORDER BY rc.data_consumo DESC;';
-  
-        // Executar a consulta
-        const [consumos] = await pool.execute(query, params);
-  
-        // Retornar resultados
-        res.json(consumos.length ? consumos : []);  // Retornar array vazio se não houver resultados
-  
-    } catch (error) {
-        console.error('Erro ao buscar consumos:', error);
-        res.status(500).json({ error: 'Erro ao buscar consumos' });
+// Endpoint para buscar
+
+consumosapp.get('/api/consumos', Autenticado, async (req, res) => {
+  try {
+    const { startDate, endDate, laboratorio } = req.query;
+
+    // Validação do formato de data (YYYY-MM-DD)
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+    if ((startDate && !dateRegex.test(startDate)) || (endDate && !dateRegex.test(endDate))) {
+      return res.status(400).json({ error: 'As datas devem estar no formato YYYY-MM-DD.' });
     }
-  });
+
+    // Validação do intervalo de datas
+    if (startDate && endDate && new Date(startDate) > new Date(endDate)) {
+      return res.status(400).json({ error: 'A data de início não pode ser posterior à data de término.' });
+    }
+
+    let query = `
+      SELECT 
+        rc.id_consumo, 
+        rc.data_consumo, 
+        e.sigla, 
+        e.nome_produto, 
+        l.nome_laboratorio, 
+        rc.quantidade, 
+        e.tipo_unidade_produto, 
+        rc.descricao 
+      FROM 
+        registro_consumo rc 
+      JOIN 
+        produto e ON rc.id_produto = e.id_produto 
+      JOIN 
+        laboratorio l ON rc.id_laboratorio = l.id_laboratorio
+    `;
+
+    const params = [];
+    const whereClauses = [];
+
+    // Filtrar por intervalo de datas
+    if (startDate && endDate) {
+      whereClauses.push('rc.data_consumo BETWEEN $1 AND $2');
+      params.push(startDate, endDate);
+    }
+
+    // Filtrar por laboratório
+    if (laboratorio && laboratorio !== 'todos') {
+      whereClauses.push(`rc.id_laboratorio = $${params.length + 1}`);
+      params.push(laboratorio);
+    }
+
+    if (whereClauses.length > 0) {
+      query += ' WHERE ' + whereClauses.join(' AND ');
+    }
+
+    query += ' ORDER BY rc.data_consumo DESC;';
+
+    // Executar a consulta
+    const { rows: consumos } = await pool.query(query, params);
+
+    // Retornar resultados
+    res.json(consumos.length ? consumos : []);  // Retornar array vazio se não houver resultados
+  } catch (error) {
+    console.error('Erro ao buscar consumos:', error);
+    res.status(500).json({ error: 'Erro ao buscar consumos' });
+  }
+});
   
   // Endpoint para buscar siglas
   app.get('/api/siglas', Autenticado, async (req, res) => {
@@ -1210,78 +1210,79 @@ app.get('/api/tabelaregistraentradaInico', Autenticado, async (req, res) => {
 });
 
 app.get('/api/tabelaregistraentrada', Autenticado, async (req, res) => {
-    try {
-        const { startDate, endDate, page = 1, limit = 20 } = req.query;
-  
-        // Validação de página e limite
-        const pageInt = parseInt(page, 10);
-        const limitInt = parseInt(limit, 10);
-  
-        if (isNaN(pageInt) || pageInt <= 0 || isNaN(limitInt) || limitInt <= 0) {
-            return res.status(400).json({ error: 'Os parâmetros de página e limite devem ser números inteiros positivos.' });
-        }
-  
-        // Limitar o limite de itens por página
-        const MAX_LIMIT = 100;
-        const finalLimit = limitInt > MAX_LIMIT ? MAX_LIMIT : limitInt;
-  
-        const offset = (pageInt - 1) * finalLimit;
-  
-        // Validação do formato das datas
-        const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
-        if ((startDate && !dateRegex.test(startDate)) || (endDate && !dateRegex.test(endDate))) {
-            return res.status(400).json({ error: 'As datas devem estar no formato YYYY-MM-DD.' });
-        }
-  
-        let query = `
-            SELECT 
-                r.id_entrada, 
-                r.data_entrada, 
-                r.quantidade, 
-                e.nome_produto, 
-                r.descricao
-            FROM registro_entrada r
-            JOIN produto e ON r.id_produto = e.id_produto
-        `;
-        
-        const params = [];
-        if (startDate && endDate) {
-            query += ' WHERE r.data_entrada BETWEEN $1 AND $2';  // Sintaxe do PostgreSQL para parâmetros
-            params.push(startDate, endDate);
-        }
-  
-        query += ' ORDER BY r.data_entrada DESC LIMIT $3 OFFSET $4'; // Sintaxe do PostgreSQL para parâmetros
-        params.push(finalLimit, offset);
-  
-        // Executar a query para obter os dados
-        const { rows } = await pool.query(query, params);
-  
-        // Obter o total de registros
-        let countQuery = `
-            SELECT COUNT(*) as total 
-            FROM registro_entrada r 
-            JOIN produto e ON r.id_produto = e.id_produto
-        `;
-  
-        if (startDate && endDate) {
-            countQuery += ' WHERE r.data_entrada BETWEEN $1 AND $2'; // Ajustando para o PostgreSQL
-        }
-  
-        const countResult = await pool.query(countQuery, startDate && endDate ? [startDate, endDate] : []);
-  
-        const totalItems = parseInt(countResult.rows[0].total, 10);
-        const totalPages = Math.ceil(totalItems / finalLimit);
-  
-        res.json({
-            data: rows,
-            totalItems,
-            totalPages,
-            currentPage: pageInt,
-        });
-    } catch (error) {
-        console.error('Erro ao buscar registros de entrada:', error);
-        res.status(500).json({ error: 'Erro ao buscar registros de entrada' });
+  try {
+    const { startDate, endDate, page = 1, limit = 20 } = req.query;
+
+    // Validação de página e limite
+    const pageInt = parseInt(page, 10);
+    const limitInt = parseInt(limit, 10);
+
+    if (isNaN(pageInt) || pageInt <= 0 || isNaN(limitInt) || limitInt <= 0) {
+      return res.status(400).json({ error: 'Os parâmetros de página e limite devem ser números inteiros positivos.' });
     }
+
+    // Limitar o limite de itens por página
+    const MAX_LIMIT = 100;
+    const finalLimit = limitInt > MAX_LIMIT ? MAX_LIMIT : limitInt;
+
+    const offset = (pageInt - 1) * finalLimit;
+
+    // Validação do formato das datas
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+    if ((startDate && !dateRegex.test(startDate)) || (endDate && !dateRegex.test(endDate))) {
+      return res.status(400).json({ error: 'As datas devem estar no formato YYYY-MM-DD.' });
+    }
+
+    let query = `
+      SELECT 
+        r.id_entrada, 
+        r.data_entrada, 
+        r.quantidade, 
+        e.nome_produto, 
+        r.descricao
+      FROM registro_entrada r
+      JOIN produto e ON r.id_produto = e.id_produto
+    `;
+    
+    const params = [];
+    if (startDate && endDate) {
+      query += ' WHERE r.data_entrada BETWEEN $1 AND $2';
+      params.push(startDate, endDate);
+    }
+
+    query += ` ORDER BY r.data_entrada DESC LIMIT $${params.length + 1} OFFSET $${params.length + 2}`;
+    params.push(finalLimit, offset);
+
+    // Executar a query para obter os dados
+    const { rows } = await pool.query(query, params);
+
+    // Obter o total de registros
+    let countQuery = `
+      SELECT COUNT(*) as total 
+      FROM registro_entrada r 
+      JOIN produto e ON r.id_produto = e.id_produto
+    `;
+
+    const countParams = [];
+    if (startDate && endDate) {
+      countQuery += ' WHERE r.data_entrada BETWEEN $1 AND $2';
+      countParams.push(startDate, endDate);
+    }
+
+    const countResult = await pool.query(countQuery, countParams);
+    const totalItems = parseInt(countResult.rows[0].total, 10);
+    const totalPages = Math.ceil(totalItems / finalLimit);
+
+    res.json({
+      data: rows,
+      totalItems,
+      totalPages,
+      currentPage: pageInt,
+    });
+  } catch (error) {
+    console.error('Erro ao buscar registros de entrada:', error);
+    res.status(500).json({ error: 'Erro ao buscar registros de entrada' });
+  }
 });
   
 app.get('/api/tabelaregistraConsumo', Autenticado, async (req, res) => {
