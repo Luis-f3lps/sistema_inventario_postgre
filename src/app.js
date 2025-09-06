15727a2b56f5ae0dbc4fd1bd8981133e77417a19
Luis Gustavo B. Santiago <74880596+Phelps671@users.noreply.github.com>
2025-09-06 00:13:51 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1539,8 +1539,8 @@ app.get("/api/requests", async (req, res) => {
          a.id_aula, u.nome as professor, l.nome_laboratorio, a.data, 
          h.hora_inicio, a.precisa_tecnico, a.status
        FROM aulas a
-       JOIN usuarios u ON a.professor_email = u.email
-       JOIN laboratorios l ON a.id_laboratorio = l.id_laboratorio
+       JOIN usuario u ON a.professor_email = u.email
+       JOIN laboratorio l ON a.id_laboratorio = l.id_laboratorio
        JOIN horarios h ON a.id_horario = h.id_horario
        WHERE 
          l.usuario_email = $1 AND a.status = 'analisando'`, // Condição correta
