1040210400368f6e3d1044dc5af1ee805858489b
Luis Gustavo B. Santiago <74880596+Phelps671@users.noreply.github.com>
2025-09-06 00:24:30 -0300

    a2

--- src/app.js
+++ src/app.js
@@ -1537,19 +1537,17 @@ app.get("/api/requests", async (req, res) => {
 
     const result = await pool.query(
       `SELECT 
-         a.id_aula, 
-         u.nome_usuario as professor,  
-         l.nome_laboratorio, 
-         a.data, 
-         h.hora_inicio, 
-         a.precisa_tecnico, 
-         a.status
+         a.id_aula, u.nome_usuario as professor, l.nome_laboratorio, 
+         a.data, h.hora_inicio, a.precisa_tecnico, a.status
        FROM aulas a
        JOIN usuario u ON a.professor_email = u.email
        JOIN laboratorio l ON a.id_laboratorio = l.id_laboratorio
        JOIN horarios h ON a.id_horario = h.id_horario
        WHERE 
-         l.usuario_email = $1 AND a.status = 'analisando'`,
+         l.usuario_email = $1 
+         AND a.status IN ('analisando', 'autorizado') 
+       ORDER BY 
+         a.data, h.hora_inicio`, // Ordena para melhor visualização
       [tecnico_email]
     );
 
